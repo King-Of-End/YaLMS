aa0320eb988e1f6a553c91e158ef570dcf75710a
King-Of-End <legobooster17@gmail.com>
2026-02-10 17:10:57 +0300

    Большая задача по Maps API. Часть №1

--- main.py
+++ main.py
@@ -1,62 +1,64 @@
+import os
 import sys
 from io import BytesIO
-from pprint import pprint
 
-from get_scaling import get_scaling
-
+from load_dotenv import load_dotenv
 import requests
-from PIL import Image
+from PyQt6.QtGui import QPixmap, QImage
+from PyQt6.QtWidgets import QMainWindow, QLabel, QWidget, QVBoxLayout, QApplication
 
+from get_scaling import get_scaling, get_coords
 
-def get_maps_params(json_response):
-    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-    toponym_coodrinates = toponym["Point"]["pos"]
-    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-    org_point = f"{toponym_longitude},{toponym_lattitude}"
+load_dotenv()
 
-    apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-    map_params = {
-        "ll": ",".join([toponym_longitude, toponym_lattitude]),
-        "spn": get_scaling(toponym),
-        "apikey": apikey,
-        'pt': "{0},pm2dgl".format(org_point)
-    }
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
 
-    return map_params
+        self.setFixedSize(600, 450)
 
+        self.label = QLabel()
+        layout.addWidget(self.label)
 
-def get_toponym():
-    toponym_to_find = " ".join(sys.argv[1:])
-    if not toponym_to_find:
-        toponym_to_find = input('Введите адрес для поиска\n')
-    if not toponym_to_find:
-        toponym_to_find = 'Барклая 5А'
+        self.load_map()
 
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    def load_map(self):
+        map_params = self.get_map_params('Байкал')
+        self.get_map(map_params)
 
-    geocoder_params = {
-        "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
-        "geocode": toponym_to_find,
-        "format": "json",
-    }
+    def get_map_params(self, name):
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+        geocoder_params = {
+            "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+            "geocode": name,
+            "format": "json"}
+        response = requests.get(geocoder_api_server, params=geocoder_params).json()
+        toponym = response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        params = {
+            'spn' : get_scaling(toponym),
+            'll': get_coords(toponym),
+            'apikey': os.getenv("API_KEY")
+        }
+        return params
 
-    response = requests.get(geocoder_api_server, params=geocoder_params)
+    def set_pixmap(self, pixmap):
+        self.label.setPixmap(pixmap)
 
-    json_response = response.json()
-    return json_response
-
-
-def main():
-    json_response = get_toponym()
-    pprint(json_response)
-    map_params = get_maps_params(json_response)
-    map_api_server = "https://static-maps.yandex.ru/v1"
-    response = requests.get(map_api_server, params=map_params)
-    im = BytesIO(response.content)
-    opened_image = Image.open(im)
-    opened_image.show()
-
+    def get_map(self, map_params):
+        map_api_server = "https://static-maps.yandex.ru/v1"
+        response = requests.get(map_api_server, params=map_params)
+        im = BytesIO(response.content).getvalue()
+        opened_image = QImage.fromData(im)
+        pixmap = QPixmap.fromImage(opened_image)
+        self.set_pixmap(pixmap)
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
